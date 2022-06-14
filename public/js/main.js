8e66ee48fe366194266a334887a8381ccac4c01d
Diego Souza <diegosouzagestor@gmail.com>
2022-06-14 17:41:28 -0300

    on change jquery

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,11 @@ var lenght = words.split(' ').length;
 var number = $("#number");
 
 number.text(lenght);
+
+var camp = $('#box-writer');
+
+camp.change(function () {
+    var content = camp.val();
+    var countWords = content.split(' ').length;
+    $('#counter-words').text(countWords);
+});
