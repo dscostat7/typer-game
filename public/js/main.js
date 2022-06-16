c93cea06faa039b4ec1716bcd5aa8338e47280ad
Diego Souza <diego.s.costa@sc.senai.br>
2022-06-16 16:41:22 -0300

    separe in functions parts of code

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 var words = $('.words').text();
 var lenght = words.split(' ').length;
 var number = $("#number");
+var timerInitial = $('#timer').text();
 
 number.text(lenght);
 
@@ -8,23 +9,36 @@ var camp = $('#box-writer');
 
 camp.on('input', function () {
     var content = camp.val();
-    var countWords = content.split(/\S+/).length -1;
+    var countWords = content.split(/\S+/).length - 1;
     var countCaracters = content.length;
 
     $('#counter-words').text(countWords);
     $('#counter-caracter').text(countCaracters);
 });
 
-var timer = $('#timer').text();
+function initialTimer() {
+    var timer = $('#timer').text();
 
-camp.one('focus', function () {
-    var interval = setInterval(function () {
-        timer--;
-        $('#timer').text(timer);
-        
-        if (timer <= 0) {
-            camp.attr('disabled', true);
-            clearInterval(interval);
-        }
-    }, 1000);
+    camp.one('focus', function () {
+        var interval = setInterval(function () {
+            timer--;
+            $('#timer').text(timer);
+
+            if (timer <= 0) {
+                camp.attr('disabled', true);
+                clearInterval(interval);
+            }
+        }, 1000);
+    });
+}
+
+var reset = $("#reset_game");
+
+reset.on('click', function () {
+    camp.attr('disabled', false);
+    camp.val('');
+    $('#counter-words').text('0');
+    $('#counter-caracter').text('0');
+    $('#timer').text(timerInitial);
+    initialTimer();
 });
