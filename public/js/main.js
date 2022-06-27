62e1e30451662b90705c5bfc89531735413bedff
Diego Souza <50335837+dscostat7@users.noreply.github.com>
2022-06-27 19:13:48 -0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -78,36 +78,3 @@ function verification() {
         }
     });
 }
-
-function inserePlacar() {
-    var corpoTable = $(".placar").find("tbody");
-    var numPalavras = $("#counter-words").text();
-    var usuario = prompt('Informe seu nome:');
-
-    var linha = novaLinha(usuario, numPalavras);
-    linha.find(".botaoRemover").click(removeLinha);
-
-    corpoTable.prepend(linha);
-}
-
-function novaLinha(usuario, numPalavras) {
-    var linha = $("<tr>");
-    var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(numPalavras);
-    var botaoRemover = $("<td>");
-    var link = $("<a>").addClass("botaoRemover").attr("href", "#");
-    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-
-    link.append(icone);
-    botaoRemover.append(link);
-    linha.append(colunaUsuario);
-    linha.append(colunaPalavras);
-    linha.append(botaoRemover);
-
-    return linha;
-}
-
-function removeLinha(event) {
-    event.preventDefault();
-    $(this).parent().parent().remove();
-}
