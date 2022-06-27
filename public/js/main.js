794cc1dcfa6b753900bce9b1b92c944dec6cb545
Diego Souza <50335837+dscostat7@users.noreply.github.com>
2022-06-27 19:09:10 -0300

    ajust functions remove lines and add placar

--- public/js/main.js
+++ public/js/main.js
@@ -83,14 +83,31 @@ function inserePlacar() {
     var corpoTable = $(".placar").find("tbody");
     var numPalavras = $("#counter-words").text();
     var usuario = prompt('Informe seu nome:');
-    var remove = "<a href='#' class='botaoRemover'><i class='small material-icons'>delete</i></a>";
 
-    var linha = "<tr>"+"<td>"+usuario+"</td>"+"<td>"+numPalavras+"</td>"+"<td>"+remove+"</td>"+"</tr>";
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botaoRemover").click(removeLinha);
 
     corpoTable.prepend(linha);
 }
 
-$(".botaoRemover").click(function(event){
+function novaLinha(usuario, numPalavras) {
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(numPalavras);
+    var botaoRemover = $("<td>");
+    var link = $("<a>").addClass("botaoRemover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+    botaoRemover.append(link);
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(botaoRemover);
+
+    return linha;
+}
+
+function removeLinha(event) {
     event.preventDefault();
     $(this).parent().parent().remove();
-});
+}
